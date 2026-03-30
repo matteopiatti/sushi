0a3972e5f4beb4ba8e990e308098812aa780f2ba
matteopiatti <github@piatti.li>
2026-03-30 18:01:29 +0200

    cleanup

--- editor/src/routes/+page.svelte
+++
@@ -1,1 +0,0 @@
-<div style="padding: 2rem; color: #999;">Select a page from the sidebar</div>
--- editor/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import { sushi } from "../src/plugin.ts";
-
-export default defineConfig({
-  plugins: [sveltekit(), sushi(process.env.CMS_USER_CWD ?? process.cwd())],
-});
